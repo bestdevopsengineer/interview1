2be5279cee711772af6bd3257badddbfd9a4e3b5
aristide dubois jou <bestdevopsengineer@gmail.com>
2026-05-12 15:17:24 -0500

    push

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -8,6 +8,15 @@ resource "aws_instance" "docker_host" {
   key_name      = data.aws_key_pair.existing.key_name
   subnet_id = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  yum install -y docker
+  systemctl enable docker
+  systemctl start docker
+  usermod -aG docker ec2-user
+  EOF
   tags = {
     Name        = var.name
     Environment = var.env
